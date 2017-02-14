34808c0fac32539520305e171aa1a520ce55170f
Dameon Orin <omegaorchid@gmail.com>
2017-02-14 21:25:43 +0000

    Add:: home html and extended base html

--- EVE_API/urls.py
+++ EVE_API/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from api import views
 
-urlpatterns = [
+admin.autodiscover()
+
+urlpatterns = {
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
-]
+}
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    # print(request.META.get('REMOTE_ADDR'))
+
+    form = "stuff"
+
+    template = 'home.html'
+    content = {'form': form}
+
+    return render(request=request, template_name=template, context=content)
