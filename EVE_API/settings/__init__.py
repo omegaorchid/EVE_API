babb7b924c37258433880266df4aab2068ff808c
Dameon Orin <omegaorchid@gmail.com>
2017-02-15 13:55:56 +0000

    Update::

--- EVE_API/settings/__init__.py
+++ EVE_API/settings/__init__.py
@@ -1,6 +1,16 @@
-from .production import *
+from .base import *
 
 try:
     from .local import *
+    live = False
 except:
-    pass
+    live = True
+if live:
+    from .production import *
+
+# from .production import *
+#
+# try:
+#     from .local import *
+# except:
+#     pass
