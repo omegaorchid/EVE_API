469381d4d894e0b0040abdb2b7fe6a5d200f966d
Dameon Orin <omegaorchid@gmail.com>
2017-02-15 13:48:30 +0000

    Update::

--- EVE_API/settings/production.py
+++ EVE_API/settings/production.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'EVE_API.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(os.path.join(BASE_DIR, 'templates'))]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
